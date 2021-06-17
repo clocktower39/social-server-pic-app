872ee36c1c5ad21d0f6c769412690b709b99e46f
clocktower39 <clocktower39@aol.com>
2021-06-17 09:23:18 -0700

    initial GridFs adjust

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const io = require('socket.io');
 const bcrypt = require('bcrypt');
-// require('dotenv').config();
+require('dotenv').config();
+const crypto = require('crypto');
+const path = require('path');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 
 console.log(process.env.YUM);
 const dbUrl = process.env.DBURL;
@@ -21,6 +27,45 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 mongoose.set('useFindAndModify', false);
+app.use(methodOverride('_method'));
+
+// connection
+const conn = mongoose.createConnection(dbUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
+let gfs;
+
+conn.once('open', () => {
+    // Init stream
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+  });
+
+// Storage
+const storage = new GridFsStorage({
+    url: dbUrl,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString("hex") + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: "uploads"
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
+  
+  const upload = multer({
+    storage
+  });
 
 let corsWhitelist = [
     'http://10.37.39.39:3001',
@@ -93,6 +138,84 @@ mongoose.connect(dbUrl,
     } , (err) => {
     console.log('mongo db connection', err)
 })
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    return res.json({src: res.req.file.filename})
+  });
+
+  
+  // get / page
+  app.get('/', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+      // Check if files
+      if (!files || files.length === 0) {
+        res.render('index', { files: false });
+      } else {
+        files.map(file => {
+          if (
+            file.contentType === 'image/jpeg' ||
+            file.contentType === 'image/png'
+          ) {
+            file.isImage = true;
+          } else {
+            file.isImage = false;
+          }
+        });
+        const readstream = gfs.createReadStream(files[files.length-1]);
+        readstream.pipe(res);
+      }
+    });
+  });
+
+  app.get('/files', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+      // Check if files
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: 'No files exist'
+        });
+      }
+  
+      // Files exist
+      return res.json(files);
+    });
+  });
+
+  app.get('/files/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+      // File exists
+      return res.json(file);
+    });
+  });
+  
+  
+  app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+  
+      // Check if image
+      if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+        // Read output to browser
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+      } else {
+        res.status(404).json({
+          err: 'Not an image'
+        });
+      }
+    });
+  });
 
 let server = http.listen(PORT, ()=> {
     console.log(`Server is listening on port ${server.address().port}`);
