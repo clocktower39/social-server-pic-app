b04d2b661a4789c73b98bf8c155df205276efcdd
clocktower39 <clocktower39@aol.com>
2022-08-22 23:57:12 -0700

    add delete like unlike comments for posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,99 +1,145 @@
-const User = require('../models/user');
-const Post = require('../models/post');
-const Relationship = require('../models/relationship');
+const User = require("../models/user");
+const Post = require("../models/post");
+const Relationship = require("../models/relationship");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
-
 const upload_post_image = (req, res) => {
-    let post = new Post(req.body);
-    post.user = res.locals.user._id;
-    post.image = res.req.file.id;
+  let post = new Post(req.body);
+  post.user = res.locals.user._id;
+  post.image = res.req.file.id;
 
-    let savePost = () => {
-        post.save((err) => {
-            if(err) {
-                res.send({err: { ...err.errors } });
-            }
-            else {
-                res.json({ src: res.req.file.filename });
-            }
-        })
-    }
-    savePost();
-}
+  let savePost = () => {
+    post.save((err) => {
+      if (err) {
+        res.send({ err: { ...err.errors } });
+      } else {
+        res.json({ src: res.req.file.filename });
+      }
+    });
+  };
+  savePost();
+};
 
 const get_post_image = (req, res) => {
-    if (req.params.id) {
-        let gridfsBucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-            bucketName: 'post'
-        });
+  if (req.params.id) {
+    let gridfsBucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+      bucketName: "post",
+    });
 
-        gridfsBucket.find({ _id: mongoose.Types.ObjectId(req.params.id) }).toArray((err, files) => {
-            // Check if files
-            if (!files || files.length === 0) {
-                return res.status(404).json({
-                    err: 'No files exist'
-                });
-            }
-
-            // Check if image
-            if (files[0].contentType === 'image/jpeg' || files[0].contentType === 'image/png') {
-                // Read output to browser
-                const readstream = gridfsBucket.openDownloadStream(files[0]._id);
-                readstream.pipe(res);
-            } else {
-                res.status(404).json({
-                    err: 'Not an image'
-                });
-            }
+    gridfsBucket.find({ _id: mongoose.Types.ObjectId(req.params.id) }).toArray((err, files) => {
+      // Check if files
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "No files exist",
         });
-    }
-    else {
+      }
+
+      // Check if image
+      if (files[0].contentType === "image/jpeg" || files[0].contentType === "image/png") {
+        // Read output to browser
+        const readstream = gridfsBucket.openDownloadStream(files[0]._id);
+        readstream.pipe(res);
+      } else {
         res.status(404).json({
-            err: 'Missing parameter',
-        })
-    }
-}
+          err: "Not an image",
+        });
+      }
+    });
+  } else {
+    res.status(404).json({
+      err: "Missing parameter",
+    });
+  }
+};
 
 const get_following_posts = async (req, res) => {
-    // Recieve list of usernames to load posts for home page
-    const followingList = await Relationship.find({ follower: res.locals.user._id });
-    let following = followingList.map(r => r.user);
-    following.push(res.locals.user._id);
+  // Recieve list of usernames to load posts for home page
+  const followingList = await Relationship.find({ follower: res.locals.user._id });
+  let following = followingList.map((r) => r.user);
+  following.push(res.locals.user._id);
 
-    const postRequest = following.map(user => {
-        return Post.find({ user })
-            .populate('user', 'username profilePicture')
-            .populate('comments', 'comment')
-            .populate('comments.user', 'username profilePicture')
-            .populate('likes', 'username profilePicture')
-            .exec();
-    })
-    const postResponse = await Promise.all(postRequest);
+  const postRequest = following.map((user) => {
+    return Post.find({ user })
+      .populate("user", "username profilePicture")
+      .populate("comments", "comment")
+      .populate("comments.user", "username profilePicture")
+      .populate("likes", "username profilePicture")
+      .exec();
+  });
+  const postResponse = await Promise.all(postRequest);
 
-    const sortedPosts = postResponse.flat().sort((a, b) => b.timestamp - a.timestamp);
-    res.send(sortedPosts);
-}
+  const sortedPosts = postResponse.flat().sort((a, b) => b.timestamp - a.timestamp);
+  res.send(sortedPosts);
+};
 
 const like_post = async (req, res) => {
-    Post.updateOne({ _id: req.body.id }, { $addToSet: { likes: res.locals.user._id } }, (err, post) => {
-        if(err) return res.send(err);
-        res.sendStatus(200);
-    })
-}
+  Post.updateOne(
+    { _id: req.body.id },
+    { $addToSet: { likes: res.locals.user._id } },
+    (err, post) => {
+      if (err) return res.send(err);
+      res.sendStatus(200);
+    }
+  );
+};
+
 const unlike_post = async (req, res) => {
-    Post.updateOne({ _id: req.body.id }, { $pull: { likes: res.locals.user._id } }, (err, post) => {
-        if(err) return res.send(err);
-        res.sendStatus(200);
-    })
-}
+  Post.updateOne({ _id: req.body.id }, { $pull: { likes: res.locals.user._id } }, (err, post) => {
+    if (err) return res.send(err);
+    res.sendStatus(200);
+  });
+};
+
+const comment_post = async (req, res) => {
+  Post.findById(req.body.id, (err, post) => {
+    if (err) return res.send(err);
+    post.comments.push({
+      user: res.locals.user._id,
+      comment: req.body.comment,
+      likes: [],
+    });
+    post.save((err, p) => {
+      if (err) return res.send(err);
+      return res.sendStatus(200);
+    });
+  });
+};
+
+const delete_comment_post = async (req, res) => {
+  Post.updateOne({ _id: req.body.id }, { $pull: { 'comments': { _id: req.body.commentId } } }, (err, post) => {
+    if (err) return res.send(err);
+    res.sendStatus(200);
+  });
+};
+
+const like_comment_post = async (req, res) => {
+  Post.updateOne(
+    { _id: req.body.id },
+    { $addToSet: { 'comments': { likes: res.locals.user._id } } },
+    (err, post) => {
+      if (err) return res.send(err);
+      res.sendStatus(200);
+    }
+  );
+};
+
+const unlike_comment_post = async (req, res) => {
+  Post.updateOne({ _id: req.body.id }, { $pull: { 'comments' : { likes: res.locals.user._id } } }, (err, post) => {
+    if (err) return res.send(err);
+    res.sendStatus(200);
+  });
+};
 
 module.exports = {
-    upload_post_image,
-    get_post_image,
-    get_following_posts,
-    like_post,
-    unlike_post,
-}
+  upload_post_image,
+  get_post_image,
+  get_following_posts,
+  like_post,
+  unlike_post,
+  comment_post,
+  delete_comment_post,
+  like_comment_post,
+  unlike_comment_post,
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -10,5 +10,9 @@ router.post('/post/upload', auth, uploadUserPost.single("file"), postController.
 router.get('/post/followingPosts', auth, postController.get_following_posts);
 router.post('/post/like', auth, postController.like_post);
 router.post('/post/unlike', auth, postController.unlike_post);
+router.post('/post/comment', auth, postController.comment_post);
+router.post('/post/deleteComment', auth, postController.delete_comment_post);
+router.post('/post/likeComment', auth, postController.like_comment_post);
+router.post('/post/unlikeComment', auth, postController.unlike_comment_post);
 
 module.exports = router;
